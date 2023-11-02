46ff6cf2964209d5ccec292648b448f99f48e5ed
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2023-11-02 14:23:19 -0500

    Remove unneeded nokogiri parsing

--- test/components/link_button_component_test.rb
+++ test/components/link_button_component_test.rb
@@ -7,9 +7,8 @@ class LinkButtonComponentTest < ActiveSupport::TestCase
   test "should render the appropriate text" do
     component = LinkButtonComponent.new(text: "Click me", path: "/")
     output = render component
-    parsed_output = Nokogiri::HTML.fragment(output)
 
-    assert_includes parsed_output.text, "Click me"
+    assert_includes output.text, "Click me"
   end
 
   class ColorsTest < ActiveSupport::TestCase
@@ -19,25 +18,22 @@ class LinkButtonComponentTest < ActiveSupport::TestCase
     test "appropriately displays success button" do
       green_component = LinkButtonComponent.new(text: "Click me", path: "/", type: Colors::SUCCESS)
       green_output = render green_component
-      parsed_green_output = Nokogiri::HTML.fragment(green_output)
 
-      assert_includes parsed_green_output.css("a").first["class"], "bg-green-500"
+      assert_includes green_output.css("a").first["class"], "bg-green-500"
     end
 
     test "appropriately displays danger button" do
       red_component = LinkButtonComponent.new(text: "Click me", path: "/", type: Colors::DANGER)
       red_output = render red_component
-      parsed_red_output = Nokogiri::HTML.fragment(red_output)
 
-      assert_includes parsed_red_output.css("a").first["class"], "bg-red-500"
+      assert_includes red_output.css("a").first["class"], "bg-red-500"
     end
 
     test "appropriately displays default button" do
       default_component = LinkButtonComponent.new(text: "Click me", path: "/")
       default_output = render default_component
-      parsed_default_output = Nokogiri::HTML.fragment(default_output)
 
-      assert_includes parsed_default_output.css("a").first["class"], "bg-gray-100"
+      assert_includes default_output.css("a").first["class"], "bg-gray-100"
     end
   end
 end
