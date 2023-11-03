16230b9d7890e4340110950da7d9b16c01dee35b
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2023-11-03 12:16:55 -0500

    Add regular view for index

--- app/views/dashboard/index_view.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class Dashboard::IndexView < ApplicationView
-  def template
-    h1 { "Dashboard::Index" }
-    p { "Find me in app/views/dashboard/index_view.rb" }
-  end
-end
