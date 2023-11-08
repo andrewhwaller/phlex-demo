6ac151423ab53598e9f6c636292066ff6c50adb0
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2023-11-08 15:59:27 -0600

    Add posts index view

--- app/views/phlex/posts/index_view.rb
+++ app/views/phlex/posts/index_view.rb
@@ -1,8 +1,31 @@
 # frozen_string_literal: true
 
 class Phlex::Posts::IndexView < ApplicationView
+  def initialize(posts:, notice: nil)
+    @posts = posts
+    @notice = notice
+  end
+
   def template
-    h1 { "Phlex::Posts::Index" }
-    p { "Find me in app/views/phlex/posts/index_view.rb" }
+    div(class: "w-full") do
+      if @notice
+        p(class: "py-2 px-3 bg-green-50 mb-50 text-green-500 font-medium rounded-lg inline-block", id: "notice") do
+          plain @notice
+        end
+      end
+      div(class: " flex justify-between items-center") do
+        h1(class: "font-bold text-4xl") { "Posts" }
+        render LinkButtonComponent.new(text: "New post", path: new_post_path)
+      end
+      div(class: "min-w-full mt-5", id: "posts") do
+        render ListComponent.new(empty_message: "No posts yet!", item_name: "post", frame_title: "posts") do |list|
+          @posts.each do |post|
+            list.item do
+              render Posts::ListItemComponent.new(post: post)
+            end
+          end
+        end
+      end
+    end
   end
 end
