7e47d00a2c1f999e6a935353267fd8a626f403c0
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2023-11-09 10:14:04 -0600

    Update list item component

--- app/views/components/posts/list_item_component.rb
+++ app/views/components/posts/list_item_component.rb
@@ -6,7 +6,7 @@ class Posts::ListItemComponent < ApplicationComponent
   end
 
   def template
-    div(class: "flex px-4 py-4 sm:px-6") do
+    div(class: "flex") do
       div(class: "flex-1") do
         h3(class: "text-lg leading-6 font-medium text-gray-900") do
           plain @post.title
@@ -16,8 +16,8 @@ class Posts::ListItemComponent < ApplicationComponent
         end
       end
       div(class: "flex gap-3 ml-5 flex-shrink-0") do
-        render LinkButtonComponent.new(text: "Edit this post", path: edit_post_path(@post))
-        render LinkButtonComponent.new(text: "Show this post", path: post_path(@post))
+        render LinkButtonComponent.new(text: "Edit this post", path: edit_phlex_post_path(@post))
+        render LinkButtonComponent.new(text: "Show this post", path: phlex_post_path(@post))
       end
     end
   end
